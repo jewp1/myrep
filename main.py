677c5d466a2681148c01eed4463db5705908e3f5
Evsyukov Sergey <evsyukovsergey01@gmail.com>
2023-09-26 23:16:47 +0300

    echo stickers

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
 BOT_TOKEN = open('token.txt').readline()
 
 # Создаем объекты бота и диспетчера
@@ -25,12 +26,28 @@ async def process_help_command(message: Message):
     )
 
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
+# хендлер для стикеров
+
+@dp.message(F.sticker)
+async def send_sticker(message: Message):
+    print(message)
+    await message.answer_sticker(message.sticker.file_id)
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
+    print(message.text)
     await message.reply(text=message.text)
 
+
+# Регистрируем хэндлеры
 
 if __name__ == '__main__':
     dp.run_polling(bot)
